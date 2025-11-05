7ecdc85bca087d7f6966b5e9460c365b7aa988ef
agvar <ajeebgeorge@gmail.com>
2025-11-05 14:39:21 -0500

    fix: fix formatting issues and created .flake8 and pyproject.toml files

--- src/APIManager.py
+++ src/APIManager.py
@@ -1,30 +1,25 @@
 import requests
-import os
-import json
-from typing import Dict,List,Optional,Any
+from typing import Dict, List, Any
 
 
 class APIManager:
     def __init__(self, header_user_agent: str, header_accept: str) -> None:
         self.session = requests.Session()
-        self.session.headers.update(
-            {"User-Agent": header_user_agent, "Accept": header_accept}
-        )
+        self.session.headers.update({"User-Agent": header_user_agent, "Accept": header_accept})
 
-    def API_get(self, url:str, params: Dict[str,Any], timeout: int) ->requests.Response:
+    def API_get(self, url: str, params: Dict[str, Any], timeout: int) -> requests.Response:
         try:
             response = self.session.get(url, params=params, timeout=timeout)
             response.raise_for_status()
             return response
-        except requests.exceptions.RequestException as e:
+        except requests.exceptions.RequestException:
             raise
 
-    def API_parse_json(self, response: requests.Response, keys:List[str]=[]) -> Dict[str,Any]:
+    def API_parse_json(self, response: requests.Response, keys: List[str] = []) -> Dict[str, Any]:
         response_json = response.json()
-        if not isinstance(response_json,dict):
-            raise ValueError(f'Expected JSON object, got {type(response_json)}')
-        
-        
+        if not isinstance(response_json, dict):
+            raise ValueError(f"Expected JSON object, got {type(response_json)}")
+
         if keys:
             return {key: response_json.get(key) for key in keys if key in response_json}
         else:
--- src/WeatherDataCollector.py
+++ src/WeatherDataCollector.py
@@ -3,9 +3,6 @@ from S3Operations import S3Operations
 from dotenv import load_dotenv
 import os
 import yaml
-import boto3
-from datetime import datetime
-import uuid
 import json
 
 
@@ -14,17 +11,13 @@ class WeatherDataCollector:
         try:
             self.api_key = self._get_api_key()
             self.config = self.__setup_config(config_path)
-            self.geocoding_url = self.config.get("app", {}).get(
-                "geocoding_by_zipcode_url"
-            )
+            self.geocoding_url = self.config.get("app", {}).get("geocoding_by_zipcode_url")
             self.weather_url = self.config.get("app", {}).get("weather_url")
             self.country_code = self.config.get("app", {}).get("ISO3166_code")
             self.zipcodes = self.config.get("app", {}).get("zipcodes")
             self.header_user_agent = self.config.get("app", {}).get("header_user_agent")
             self.header_accept = self.config.get("app", {}).get("header_accept")
-            self.source_bucket = (
-                self.config.get("s3", {}).get("buckets", {}).get("source_bucket")
-            )
+            self.source_bucket = self.config.get("s3", {}).get("buckets", {}).get("source_bucket")
             self.region = self.config.get("aws", {}).get("region", "us-east-1")
             self.TIMEOUT = 10
             apiManager = APIManager(self.header_user_agent, self.header_accept)
@@ -37,9 +30,7 @@ class WeatherDataCollector:
                     "zip": f"{zipcode},{self.country_code}",
                     "appid": self.api_key,
                 }
-                geo_response = apiManager.API_get(
-                    self.geocoding_url, geo_params, self.TIMEOUT
-                )
+                geo_response = apiManager.API_get(self.geocoding_url, geo_params, self.TIMEOUT)
                 geo_response_json = apiManager.API_parse_json(geo_response)
 
                 lat = geo_response_json.get("lat")
@@ -59,10 +50,10 @@ class WeatherDataCollector:
                     self.weather_url, weather_params, self.TIMEOUT
                 )
                 weather_json_response = apiManager.API_parse_json(weather_response)
-                s3_response = s3Operations.store_object_in_s3(
+                s3Operations.store_object_in_s3(
                     self.source_bucket, zipcode, json.dumps(weather_json_response)
                 )
-        except Exception as e:
+        except Exception:
             raise
 
     def __setup_config(self, config_path):
