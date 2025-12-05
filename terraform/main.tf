dc1a14f996c031e03fd1168b818701a0e228852f
agvar <ajeebgeorge@gmail.com>
2025-12-05 15:17:10 -0500

    fix: Updated comments on main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,9 +12,9 @@ data "aws_iam_role" "github_actions_aws"{
   name="github-actions-weather-app"
 }
 
-#################################
+############################
 # Refer Persistent resources
-#################################
+############################
 
 data "aws_s3_bucket" "weatherDataStore"{
   bucket = "weather-data-store-bucket-11-07-2025-12-20-avar"
