f4cf837a6b1ba1b3cc44e7da873cdcc633fbc20d
agvar <ajeebgeorge@gmail.com>
2025-12-05 20:42:51 -0500

    fix: Add depends on property on aws_cloudwatch_event_rule for target

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,6 +50,7 @@ resource "aws_cloudwatch_event_rule" "weather_collection_schedule" {
   name  = "weather-collection-schedule"
   description         = "Trigger weather collection twice daily"
   schedule_expression = "cron(0 6,18 * * ? *)"
+  depends_on = [aws_cloudwatch_event_target.trigger_lambda]
 }
 
 resource "aws_cloudwatch_event_target" "trigger_lambda"{
